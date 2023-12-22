c2a2011993cab1a640a9d67261ec46e88a6ade24
Oepfelmues <benjamin@stupf.ch>
2023-12-22 21:39:57 +0100

    Layout von Bootstap angepasst und eingefügt

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -11,22 +11,20 @@ export default {
 };
 </script>
 <template>
-  <div class="card mb-3" style="max-width: 540px">
-    <div class="row no-gutters">
-      <div class="col-md-4">
-        <img :src="data.imageUrl" class="card-img" alt="..." />
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">{{ data.productName }}</h5>
-          <Rating :initalRating="data.rating" />
-          <p class="card-text">{{ data.productPrice }}</p>
-          <p class="card-text">{{ data.description }}</p>
-          <p class="card-text">
-            <small class="text-muted">{{ data.releasedDate }}</small>
-          </p>
-        </div>
-      </div>
+  <div class="card-deck">
+  <div class="card">
+    <img :src="data.imageUrl" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{ data.productName }}</h5>
+      <p class="card-text">{{ data.productPrice }}</p>
+      <p class="card-text">{{ data.description }}</p>
+      <Rating :initalRating="data.rating" />
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated {{ data.releasedDate }} ago</small>
     </div>
   </div>
+  </div>
 </template>
+
+
